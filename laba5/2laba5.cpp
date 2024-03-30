2d367cf5cefb4e1e62c5eb32cec80e37909077ff
ElenaYurova21 <59369268+hellyu21@users.noreply.github.com>
2024-03-30 11:19:41 +0200

    Create 2laba5.cpp

---
+++ laba5/2laba5.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+using namespace std;
+
+class R_Complex {
+	float r;
+	float y;
+	float pi = cos(-1);
+
+public:
+	//по умолчанию
+	R_Complex() = default;
+
+	//полного заполнения
+	R_Complex(float gr, float gy)
+	{
+		r = gr;
+		y = gy;
+	}
+
+	//копирования
+	R_Complex(const R_Complex& other) {
+		r = other.r;
+		y = other.y;
+	}
+
+	//присваивания копированием
+	R_Complex& operator = (const R_Complex& other) {
+		R_Complex new(0,0);
+	}
+
+	//деструктор
+	~R_Complex() {
+		cout << "Class deleted" << endl;
+	}
+
+	//операторы += + *=
+	//
+
+	//оператор ++
+	//
+
+	//оператор ввода
+	void set() {
+		cin >> r;
+		cin >> y;
+	}
+
+	//оператор вывода
+	void print() {
+		cout << r << "*(cos(" << y << ") + i*sin(" << y << "))" << endl;
+	}
+};
+
+int main(){
+	R_Complex roar(0,0);
+	roar.set();
+	roar.print();
+	return 0;
+}
